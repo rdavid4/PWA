720ecde534bcc879931b21fe9a7c375613d414b4
Roger Quiñonez <rogerdavid444@gmail.com>
2019-08-15 19:41:46 +0200

    ciclo de vida del service worker

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,26 @@
 
 // Ciclo de vida del SW
 
+//Instalacion de SW
+self.addEventListener('install', event => {
+    // Descargar Assets
+    // Creamos un cache
+    console.log('Instalando SW')
+    const instalacion = new Promise( (resolve, reject) =>{
+        //activar nuevo SW
+        self.skipWaiting()
+        console.log('Instalación terminada')
+        resolve()
+    })
+
+    event.waitUntil(instalacion)
+   
+
+})
+
+
+//Activacion de SW
+self.addEventListener('activate', event => {
+    //Borrar cache viejo
+    console.log('SW activo y listo');
+})
